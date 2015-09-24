3a6fb037c63cdbb682be22393bcb36671db08a83
xdsnet <xiongds@scude.cc>
2015-09-24 16:07:07 +0800

    添加测试题样例，并补充修改项目说明

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var config={xmlurl:'./tets1.xml',     //定义导入题库xml
+var config={xmlurl:'./test1.xml',     //定义导入题库xml
       tirand:true,                      //在同类型题中是否允许打乱题序
       xrand:true,                       //是否允许打乱选项顺序，判断题不打乱
       t4rand:false                     //是否允许打乱分析题的题序（因为分析题可能主体部分相同，有多问情况，这时就不能打乱）
